31a518504640beb4897d0b9f9e50a2a9196e75ba
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2024-12-23 19:07:34 +0100

    Replace toml parsing library (#32)
    "toml" does not support all TOML features
    https://github.com/BinaryMuse/toml-node/issues/51
    
    Fixes: #31

--- src/utils/pyproject.ts
+++ src/utils/pyproject.ts
@@ -1,15 +1,18 @@
 import * as fs from "node:fs";
 import * as core from "@actions/core";
-import * as toml from "toml";
+import * as toml from "@iarna/toml";
 
 export function getRuffVersionFromPyproject(
   filePath: string,
 ): string | undefined {
   const pyprojectContent = fs.readFileSync(filePath, "utf-8");
-  const pyproject = toml.parse(pyprojectContent);
+  const pyproject = toml.parse(pyprojectContent) as {
+    project?: { dependencies?: string[] };
+    "dependency-groups"?: { dev?: string[] };
+  };
 
-  const dependencies: string[] = pyproject.project?.dependencies || [];
-  const devDependencies: string[] = pyproject["dependency-groups"]?.dev || [];
+  const dependencies: string[] = pyproject?.project?.dependencies || [];
+  const devDependencies: string[] = pyproject?.["dependency-groups"]?.dev || [];
 
   const ruffVersionDefinition =
     dependencies.find((dep: string) => dep.startsWith("ruff")) ||
